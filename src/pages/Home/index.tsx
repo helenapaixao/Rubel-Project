0a900afef805161b621dc4071d42ac8bd4dd4f5d
helenapaixao <hp.helenapaixao@gmail.com>
2021-09-10 00:19:14 -0300

    feat: textdisplay ok

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,35 @@
 import React,{useEffect, useState} from 'react';
 import api from '../../services/api'
 
+interface IComments {
+  textDisplay: string,
+}
+
 export const Home = () => {
-  const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState<IComments| null>(null);
 
   useEffect(() => {
     api.get("commentThreads?key=AIzaSyBVvjnVOq46KUN693RBjLFCORCu2ODf8rw&textFormat=plainText&part=snippet&videoId=tMWpm_GOLaA&maxResults=50").then(response => {
-      console.log(response.data)
+      console.log(response.data.items[0].snippet.topLevelComment.snippet.textDisplay);
+      setComments(response.data.items[0].snippet.topLevelComment.snippet);
     })
 
   }, [comments]);
 
-
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+  <>
+  
+     
+      <div>
+        {comments && (
+                <h1>{comments?.textDisplay}</h1>     
+          )}
+       
+
+        </div>
+    
+    </>
+  
   )
 }
 
