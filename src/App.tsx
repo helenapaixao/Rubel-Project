c396ef3c2e02fd64ee1c9451e17e606bbf09ebcc
helenapaixao <hp.helenapaixao@gmail.com>
2023-06-25 18:59:59 -0300

    fix: config code

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import GlobalStyles from "./styles/global";
 import { ChakraProvider } from "@chakra-ui/react"
 import Home from "../src/pages/Home";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
       <ChakraProvider>
       <GlobalStyles />
       <Home />
+      <Analytics/>
       </ChakraProvider>
   );
 }
